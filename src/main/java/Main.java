bea5f424edb038101cee10db1945655700d498ba
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-09-30 13:45:57 -0500

    Maven exercise completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello there");
+        public static void main(String[] args) {
+            Scanner scs = new Scanner(System.in);
+            System.out.print("Input: ");
+            String input = scs.nextLine();
+            System.out.println("You entered: \"" + input + "\"");
+            System.out.println("\"" + input + "\" " + returnNumMessage(input));
+            System.out.println("Flipped Case: " + StringUtils.swapCase(input));
+            System.out.println("Reversed: " + StringUtils.reverse(input));
+        }
+        public static String returnNumMessage(String input) {
+            return (StringUtils.isNumeric(input)) ? "is a number" : "is not a number";
+        }
     }
-}
